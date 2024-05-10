50bf0ad7c85dd66d1c42f3eca63911559886d6fd
GoGradually <chkun3109@gmail.com>
2024-05-10 20:04:53 +0900

    주석 작성

--- Agricola/entity/farm/arable_land.py
+++ Agricola/entity/farm/arable_land.py
@@ -1,7 +1,14 @@
+from entity.farm.crop_type import CropType
 from entity.farm.field import Field
 
-
+'''
+경작지
+position : 위치
+type : 쌓인 작물의 종류
+stack : 쌓인 작물의 갯수
+'''
 class ArableLand(Field):
     def __init__(self, position):
         self._position = position
         self._stack = 0
+        self._type = CropType.NONE
--- Agricola/entity/farm/cage.py
+++ Agricola/entity/farm/cage.py
@@ -1,7 +1,12 @@
 from entity.farm.animal_type import AnimalType
 from entity.farm.field import Field
 
-
+'''
+울타리 정보
+한 칸별로 생성
+한칸당 최대 2마리까지 수용
+외양간 존재 시 4마리까지 수용
+'''
 class Cage(Field):
     def __init__(self, position):
         self._kind = AnimalType.NONE
