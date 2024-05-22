cffcc95010d66bce141b913fd4041c6b778bcbbf
GoGradually <chkun3109@gmail.com>
2024-05-22 23:48:21 +0900

    test 폴더 이름 변경

---
+++ Agricola/test/animal_move_test.py
@@ -0,0 +1,80 @@
+from behavior.basebehavior.animal_move_validation import AnimalMoveValidation
+
+from entity.animal_type import AnimalType
+from entity.farm.arable_land import ArableLand
+from entity.farm.cage import Cage
+import pytest
+
+
+class TestAnimalMove:
+    def test_animal_move(self):
+        # 필드 상태 예시 데이터
+        field_status = [[Cage() for _ in range(11)] for _ in range(7)]
+        # 동물의 위치 설정
+        position = (2, 4)  # 3행 5열 위치
+        # 동물 종류 설정
+        animal_type = AnimalType.COW  # 예를 들어 소
+
+        # AnimalMoveValidation 인스턴스 생성
+        validator = AnimalMoveValidation(field_status, animal_type, position)
+
+        # 실행 메소드 호출
+        move_possible = validator.execute()
+
+        assert move_possible == True
+
+    def test_animal_move_on_structure(self):
+        # 필드 상태 예시 데이터
+        field_status = [[Cage() for _ in range(11)] for _ in range(7)]
+        # 동물의 위치 설정
+        position = (2, 4)  # 3행 5열 위치
+
+        field_status[5][9] = ArableLand()
+        # 동물 종류 설정
+        animal_type = AnimalType.COW  # 예를 들어 소
+
+        # AnimalMoveValidation 인스턴스 생성
+        validator = AnimalMoveValidation(field_status, animal_type, position)
+
+        # 실행 메소드 호출
+        move_possible = validator.execute()
+
+        assert move_possible == False
+
+    def test_animal_move_on_diff_animal(self):
+        # 필드 상태 예시 데이터
+        field_status = [[Cage() for _ in range(11)] for _ in range(7)]
+        # 동물의 위치 설정
+        position = (2, 4)  # 3행 5열 위치
+
+        # 기존 동물 : 돼지
+        field_status[5][9].kind = AnimalType.PIG
+        # 동물 종류 설정
+        animal_type = AnimalType.COW  # 예를 들어 소
+
+        # AnimalMoveValidation 인스턴스 생성
+        validator = AnimalMoveValidation(field_status, animal_type, position)
+
+        # 실행 메소드 호출
+        move_possible = validator.execute()
+
+        assert move_possible == False
+
+    def test_animal_move_next_diff_animal(self):
+        # 필드 상태 예시 데이터
+        field_status = [[Cage() for _ in range(11)] for _ in range(7)]
+        # 동물의 위치 설정
+        position = (2, 4)  # 3행 5열 위치
+
+        # 기존 동물 : 돼지
+        field_status[5][7].kind = AnimalType.PIG
+        # 동물 종류 설정
+        animal_type = AnimalType.COW  # 예를 들어 소
+
+        # AnimalMoveValidation 인스턴스 생성
+        validator = AnimalMoveValidation(field_status, animal_type, position)
+
+        # 실행 메소드 호출
+        move_possible = validator.execute()
+
+        assert move_possible == False
