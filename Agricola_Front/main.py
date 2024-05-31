8cc2d8e8767d4fcc65f8b6232f74b8d04a3084eb
kim_mon_ey <113882868+kimmoney@users.noreply.github.com>
2024-06-01 05:33:52 +0900

    메인윈도우 항목 추가 및 메인 stacked 애니메이션 구현
    메인 윈도우 layout으로 수정
    메인stacked추가 및 함수 구현
    - 오른쪽 버튼으로 테스트
    - change_stacked_page의 after_page는 stacked widget의 page name을 변수로
    각 위젯에 pressed Event 적용 등

--- Agricola_Front/main.py
+++ Agricola_Front/main.py
@@ -1,16 +1,22 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import QTimer
+
 import os
 import images_rc
+import IMG_0404_rc
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-personal_field_ui = uic.loadUiType(resource_path("PersonalField/field_frame.ui"))[0] # 농장 15개 빈칸 뚫린 ui
 main = uic.loadUiType(resource_path("mainwindow_v1.ui"))[0] # 진짜 메인
 field_base_ui = uic.loadUiType(resource_path("PersonalField/field_base.ui"))[0] # field 하나 ui
+personal_field_ui = uic.loadUiType(resource_path("PersonalField/field_frame.ui"))[0] # 농장 15개 빈칸 뚫린 ui
+personal_resourcs_ui= uic.loadUiType(resource_path("PersonalField/personal_resource.ui"))[0] # 농장 15개 빈칸 뚫린 ui
+personal_card_ui= uic.loadUiType(resource_path("PersonalField/personal_card.ui"))[0] # 농장 15개 빈칸 뚫린 ui
+basic_roundcard_ui= uic.loadUiType(resource_path("Basic/round_stack.ui"))[0] # 농장 15개 빈칸 뚫린 ui
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 # field_0_ui
@@ -21,29 +27,90 @@ class MainWindowClass(QMainWindow, main) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
-        self.personal_field_0 = WidgetPersonalField(0) # p0의 개인농장
-        self.personal_field_1 = WidgetPersonalField(1)
-        self.personal_field_2 = WidgetPersonalField(2)
-        self.personal_field_3 = WidgetPersonalField(3)
-        self.personal_field = [WidgetPersonalField(i) for i in range(4)]
-        self.hlo_p0_0.addWidget(self.personal_field_0)
-        self.hlo_p2_0.addWidget(self.personal_field_2)
+        #플레이어 필드 위젯 설정
+        self.personal_field = [WidgetPersonalField(i,self) for i in range(4)]
+        for i in range(4):getattr(self,f"frm_p{i}_0").addWidget(self.personal_field[i])
+        #메인 필드 위젯 설정
+        self.main_field = WidgetPersonalField(5,self)
+        self.frm_main_field.addWidget(self.main_field)
+        #플레이어 카드 위젯 설정
+        self.personal_card = [WidgetPersonalCard(i,self) for i in range(4)]
+        for i in range(4):getattr(self,f"frm_p{i}_1").addWidget(self.personal_card[i])
+
+        #메인 카드 위젯 설정
+        self.main_card = WidgetPersonalCard(5,self)
+        self.frm_main_card.addWidget(self.main_card)
+        #플레이어 리소스 위젯 설정
+        self.personal_resource = [WidgetPersonalResource(i,self) for i in range(4)]
+        for i in range(4):getattr(self,f"frm_p{i}_2").addWidget(self.personal_resource[i])
+        #베이직 라운드 위젯 설정
+        self.basic_round = [WidgetBasicRound(i,self) for i in range(16)]
+        for i in range(16):getattr(self,f"roundCard_{i}").addWidget(self.basic_round[i])
+
+
+        ####################################init####################################
+        self.timer_close,self.timer_open = QTimer(self),QTimer(self)
+        self.log.clicked.connect(self.change_main)
+        ############################################################################
 
 
 
+    def change_main(self):
+        currentWidget = self.stackedWidget.currentWidget().objectName()
+        # if index == 0:self.stackedWidget.setCurrentIndex(1)
+        # else:self.stackedWidget.setCurrentIndex(0)
+        if currentWidget == "round":self.change_stacked_page("personal")
+        else:self.change_stacked_page("round")
+
+
+
+    def change_stacked_page(self,after_page):
+        after_page = getattr(self,after_page)
+        stacked_Widget = self.stackedWidget
+        if not self.timer_close.isActive() and not self.timer_open.isActive():
+            # 이걸로 속도 조절 낮을수록 빠름
+            self.speed = 5
+            self.timer_close = QTimer(self)
+            self.timer_open = QTimer(self)
+            self.total_timer_count = 20
+            self.current_timer_count = 0
+            self.timer_open.stop()
+            self.timer_close.stop()
+            self.timer_close.timeout.connect(lambda : process_timer_close(self,after_page))
+            self.timer_close.start(self.speed)
+            self.timer_open.timeout.connect(lambda : process_timer_open(self,after_page))
+        def process_timer_close(self,after_page):
+            self.opacity_effect = QGraphicsOpacityEffect(stacked_Widget.currentWidget())
+            self.opacity_effect_after = QGraphicsOpacityEffect(after_page)
+            self.opacity_effect_after.setOpacity(0)
+            self.opacity_effect.setOpacity(1-0.05*self.current_timer_count)
+            stacked_Widget.currentWidget().setGraphicsEffect(self.opacity_effect)
+            self.current_timer_count += 1
+            if self.current_timer_count == self.total_timer_count:
+                stacked_Widget.setCurrentWidget(after_page)
+                while not after_page.isVisible():
+                    pass
+                # self.title.setText(title)
+                self.timer_open.start(self.speed)
+                self.timer_close.stop()
+                self.opacity_effect_after.setOpacity(1)
+        def process_timer_open(self,after_page):
+            self.opacity_effect_after.setOpacity(1-0.05*self.current_timer_count)
+            after_page.setGraphicsEffect(self.opacity_effect_after)
+            self.current_timer_count -= 1
+            if self.current_timer_count == 0:
+                self.timer_open.stop()
 
 
 
 class WidgetPersonalField(QWidget, personal_field_ui) :
-    def __init__(self, player) :
+    def __init__(self, player,parent) :
         super().__init__()
         self.setupUi(self)
         self.player = player
-        # field
-        field_list = [setattr(self, f"field_{i}", widgetFieldBase(i,self)) for i in range(15)]
+        self.main = parent
         for i in range(15):
-            setattr(self, f"field_{i}", widgetFieldBase(i,self)) # field_0 ~ field_14 까지
-            #field_list.append(getattr(self, f"field_{i}"))
+            setattr(self, f"field_{i}", self.widgetFieldBase(i,self)) # field_0 ~ field_14 까지
         
         tmp_field_num = 0
         for i in range(3):
@@ -61,21 +128,54 @@ class WidgetPersonalField(QWidget, personal_field_ui) :
     
     def print_id(self, id):
         print(f"Player ID : {self.player} | Fence ID: {id}")
+        print(self.objectName())
 
-class widgetFieldBase(QWidget, field_base_ui) :
-    def __init__(self, id,parent):
-        super().__init__()
-        self.setupUi(self)
-        self.id = id # field에게 고유 id (0~14) 부여
+    class widgetFieldBase(QWidget, field_base_ui) :
+        def __init__(self, id,parent):
+            super().__init__()
+            self.setupUi(self)
+            self.id = id # field에게 고유 id (0~14) 부여
+            self.parent = parent
+            self.btn_field_unit.clicked.connect(self.print_id)
+        def mousePressEvent(self,event):
+            print(f"Pressed Fance Player ID : {self.parent.player} | Fence ID: {self.id}")
+
+        def print_id(self):
+            print(f"Player ID : {self.parent.player} | Fence ID: {self.id}")
+            if not self.pushButton_2.isVisible():
+                self.pushButton_3.hide()
+            self.pushButton_2.hide()
+
+class WidgetPersonalCard(QWidget, personal_card_ui) :
+    def __init__(self, player,parent) :
+        super().__init__()  # 부모 클래스의 __init__ 함수 호출
         self.parent = parent
-        self.btn_field_unit.clicked.connect(self.print_id)
+        self.player = player
+        self.setupUi(self)
     def mousePressEvent(self,event):
-        print(self.parent.player, self.id)
-    def print_id(self):
-        print(f"Field ID:{self.parent.player} {self.id}")
-        if not self.pushButton_2.isVisible():
-            self.pushButton_3.hide()
-        self.pushButton_2.hide()
+        print(f"Pressed card Player ID : {self.player}")
+
+class WidgetPersonalResource(QWidget, personal_resourcs_ui) :
+    def __init__(self, player,parent) :
+        super().__init__()  # 부모 클래스의 __init__ 함수 호출
+        self.setupUi(self)
+        self.player = player
+        self.parent = parent
+    def mousePressEvent(self,event):
+        print(f"Pressed Resource Player ID : {self.player}")
+        index = self.stackedWidget.currentIndex()
+        if index == 0:self.stackedWidget.setCurrentIndex(1)
+        else:self.stackedWidget.setCurrentIndex(0)
+
+class WidgetBasicRound(QWidget, basic_roundcard_ui) :
+    def __init__(self, round,parent) :
+        super().__init__()  # 부모 클래스의 __init__ 함수 호출
+        self.parent = parent
+        self.round = round
+        self.setupUi(self)
+    def mousePressEvent(self,event):
+        print(f"Pressed basic round ID : {self.round}")
+
 ###실행 코드### 밑에 건들 필요 굳이 없음###
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
