4da8b8ad938b817686d018f8ec73701bdfb81954
GoGradually <chkun3109@gmail.com>
2024-06-01 00:26:00 +0900

    Oven 1 인터페이스 적용

--- Agricola/behavior/main_facility/oven1.py
+++ Agricola/behavior/main_facility/oven1.py
@@ -1,3 +1,17 @@
 """
 흙 2개 화로
 """
+
+
+from behavior.main_facility.main_facility_interface import MainFacilityInterface
+
+
+class Oven1(MainFacilityInterface):
+    def execute(self):
+        pass
+
+    def log(self):
+        pass
+
+    def purchase(self):
+        pass
