ebd5a6ca6c6212bad9ed1367acce0ebf364612ca
GoGradually <chkun3109@gmail.com>
2024-06-06 11:03:32 +0900

    시작 카드 분배 기능 구현

--- Agricola/gameready/card_distribution.py
+++ Agricola/gameready/card_distribution.py
@@ -1,12 +1,23 @@
 """
 카드 분배 커맨드
+각 player_status 의 card 객체에 6장의 카드 번호를 넣는다.
 """
+import random
+
 from command import Command
+from repository.player_status_repository import player_status_repository
 
 
 class CardDistribution(Command):
+
     def execute(self):
-        pass
+        card_list = random.sample(range(24), 24)
+        for i in range(4):
+            for j in range(3):
+                player_status_repository.player_status[i].card.handSubCard.append(card_list[i * 3 + j])
+        for i in range(4):
+            for j in range(3):
+                player_status_repository.player_status[i].card.handJobCard.append(card_list[12 + i * 3 + j])
 
     def log(self):
         pass
