f616be59c093af8728804a72f4e697ad02d9cabf
JongHun, Jang <55573969+KKIIO21@users.noreply.github.com>
2024-06-06 14:48:05 +0900

    type추가

---
+++ Agricola/entity/main_facility_type.py
@@ -0,0 +1,13 @@
+"""
+주요 설비 Enum
+"""
+
+from enum import Enum
+
+
+class MainFacilityType(Enum):
+    DIRT_KILN = 0
+    OVEN1 = 1
+    OVEN2 = 2
+    STRONG_OVEN1 = 3
+    STRONG_OVEN2 = 4
---
+++ Agricola/entity/sub_facility_type.py
@@ -0,0 +1,21 @@
+"""
+보조 설비 Enum
+"""
+
+from enum import Enum
+
+
+class SubFacilityType(Enum):
+    BASKET = 0
+    BOTTLE = 1
+    CANOE = 2
+    GIANT_FARM = 3
+    GRAIN_SHOVEL = 4
+    JUNK_WAREHOUSE = 5
+    LOAM_MINING_SITE = 6
+    MANAGER = 7
+    PINCER = 8
+    PITCHFORK = 9
+    SILPAN = 10
+    WOOL = 11
+    
