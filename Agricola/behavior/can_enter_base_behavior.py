4141cb4f372a4702cd0de13a311f8ad6d5a5fe77
GoGradually <chkun3109@gmail.com>
2024-06-07 14:13:33 +0900

    행동 진입 가능 여부 검증 커맨드 구현

--- Agricola/behavior/can_enter_base_behavior.py
+++ Agricola/behavior/can_enter_base_behavior.py
@@ -3,14 +3,14 @@ from repository.game_status_repository import game_status_repository
 from repository.round_status_repository import round_status_repository
 
 
-class CanEnterRoundBehavior(Command):
-    def __init__(self, behavior, behavior_enum):
+class CanEnterBaseBehavior(Command):
+    def __init__(self, behavior, behavior_index):
         self.log_text = ""
         self.behavior = behavior
-        self.behavior_enum = behavior_enum
+        self.behavior_index = behavior_index
 
     def execute(self):
-        if not round_status_repository.round_status.put_basic[self.behavior_enum] and \
+        if round_status_repository.round_status.put_basic[self.behavior_index] == -1 and \
                 self.behavior.can_play():
             self.log_text = "행동이 수행가능합니다."
             return True
