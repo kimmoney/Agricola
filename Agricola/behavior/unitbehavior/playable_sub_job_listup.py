042a8242736fbd724e25fb54990020e12c458248
GoGradually <chkun3109@gmail.com>
2024-06-07 23:52:17 +0900

    카드 리스트업 함수 수정 -> 매개변수 안받도록

--- Agricola/behavior/unitbehavior/playable_sub_job_listup.py
+++ Agricola/behavior/unitbehavior/playable_sub_job_listup.py
@@ -5,12 +5,14 @@
 :rtype: list<card_name>
 """
 from command import Command
+from repository.game_status_repository import game_status_repository
 from repository.player_status_repository import player_status_repository
 
 
 class PlayableJobCardListup(Command):
-    def __init__(self, player):
+    def __init__(self):
         self.log_text = ""
+        player = game_status_repository.game_status
         self.jobList = self.player_ownCard = player_status_repository.player_status[player].own_card.hand_job_card
 
     def execute(self):
